f4a17312a71d8c7e827b3667fd01cd6b2e6ad58e
MohammadAbidShah <shahaabid021@gmail.com>
2026-08-14 16:05:59 +0530

    Add first pytest test for drop_incomplete_rows

---
+++ tests/test_transform.py
@@ -0,0 +1,16 @@
+import sys
+sys.path.append("src")
+
+import pandas as pd
+from transform import drop_incomplete_rows
+
+def test_drop_incomplete_rows():
+    data = {
+        "quantity": [3, None, 5],
+        "unit_price": [10.0, 20.0, None]
+    }
+    df = pd.DataFrame(data)
+
+    result = drop_incomplete_rows(df)
+
+    assert len(result) == 1
